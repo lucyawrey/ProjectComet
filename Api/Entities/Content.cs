6611ea44b8ecd2c91250e67c843ed9b3440e20b7
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2025-09-13 22:12:54 -0400

    Update Content.cs

--- Api/Entities/Content.cs
+++ Api/Entities/Content.cs
@@ -4,4 +4,23 @@ public class Content : IUpdated, IGeneratedId
 {
     public long Id { get; set; } = 0;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public required string Name { get; set; }
+    public required ContentType ContentType { get; set; }
+    public required ContentSubtype ContentSubtype { get; set; }
+    public required ContentData ContentData { get; set; }
+}
+
+public class ContentData
+{
+
+}
+
+public enum ContentType
+{
+    Item = 0, Companion = 1, Unlock = 2
+}
+
+public enum ContentSubtype
+{
+
 }
